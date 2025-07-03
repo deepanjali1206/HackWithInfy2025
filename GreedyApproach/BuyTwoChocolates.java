3f959c3cc8628539abe0fb3ce4e2c0e327620c87
deepa <deepanjalisingh0612@gmail.com>
2025-07-03 09:46:46 +0530

    2 Approach

--- GreedyApproach/BuyTwoChocolates.java
+++ GreedyApproach/BuyTwoChocolates.java
@@ -11,11 +11,31 @@ public class BuyTwoChocolates {
     }
 
     public static int buy(int price[], int cost) {
-        Arrays.sort(price);
-        int total = price[0] + price[1];
+        // Arrays.sort(price);
+        // int total = price[0] + price[1];
+        // if (total <= cost) {
+        // return cost - total;
+
+        // } else {
+        // return cost;
+        // }
+        // }
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int i = 1; i < price.length; i++) {
+            if (price[i] < min1) {
+                min2 = min1;
+                min1 = price[i];
+
+            } else if (price[i] < min2) {
+                min2 = price[i];
+            }
+        }
+        int total = min1 + min2;
+
         if (total <= cost) {
             return cost - total;
-
         } else {
             return cost;
         }
