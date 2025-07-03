78747d8186b7164a8a69559ae3334c07707e3d87
deepa <deepanjalisingh0612@gmail.com>
2025-07-03 10:44:11 +0530

    DI string Match code

---
+++ GreedyApproach/DIStringMatch.java
@@ -0,0 +1,31 @@
+package GreedyApproach;
+
+import java.util.*;
+
+public class DIStringMatch {
+    public static int[] stringMatch(String s) {
+        int n = s.length();
+        int low = 0;
+        int high = n;
+        int[] res = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                res[i] = low;
+                low++;
+            } else {
+                res[i] = high;
+                high--;
+            }
+        }
+        res[n] = low;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int[] res = stringMatch(s);
+        System.out.println(Arrays.toString(res));
+    }
+}
